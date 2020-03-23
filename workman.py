76efbd79e476b87518f8ed8edcca5ec7cba6a5ac
Glitch (hello-webpage) <none>
2020-03-23 17:51:37 +0000

    🚎🏍 Checkpoint ./workman.py:5152800/658

---
+++ workman.py
@@ -0,0 +1,15 @@
+# workman 1.0.0 for python 3.0.0 +
+
+from sys import argv, dont_write_bytecode
+dont_write_bytecode = True
+from time import *
+import os
+
+print("WORKMAN PACKAGE MANAGER FOR PYTHON")
+print("----------------------------------")
+print("version = version info")
+print("creators = creator info")
+print("forget = delete the entire manager (not packages)")
+print("-------------------")
+
+cli = input("workman>> ")
