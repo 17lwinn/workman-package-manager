b93d25202110d5db7875b9126ba0a7e73646096f
Glitch (hello-webpage) <none>
2020-03-24 08:03:48 +0000

    👗🎸 Checkpoint ./workman.py:5152800/1286

--- workman.py
+++ workman.py
@@ -11,5 +11,32 @@ print("version = version info")
 print("creators = creator info")
 print("forget = delete the entire manager (not packages)")
 print("-------------------")
+print("install: installs packages, on prompt type the EXACT PACKAGE NAME!")
+print("uninstall: deletes packages, on prompt type the EXACT PACKAGE NAME!")
 
 cli = input("workman>> ")
+
+if cli == "version": # version number
+  print("workman ver. 1.0.0 for python 3.0.0")
+  
+if cli == "creators": # creators!
+  print("ProTechCEO, i think")
+  
+if cli == "forget": # deletes this manager
+  forget = input("this will delete the manager, you can always reinstall. Continue? Y/N: ")
+  
+  if forget == "Y":
+    print("bye bye, nice knowing you")
+    sleep(2)
+    os.remove("workman.py")
+    print("workman has been deleted")
+    
+  if forget ==  "N":
+    os.system("python3 workman.py")
+    
+# package management ----------------------------------------------------------
+
+if cli == "install":
+  install = input("install: ")
+  
+  os.system("wget https://workman-source.glitch.me/" + install + ".zip")
